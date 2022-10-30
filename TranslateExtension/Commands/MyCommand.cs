57e337aefddbbebdcab752698e5fe4f401aabedc
Vladyslav Hlushkov <v.glushkov@mobidev.biz>
2022-10-30 22:29:29 +0200

    Add project files.

---
+++ TranslateExtension/Commands/MyCommand.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.Text;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TranslateExtension
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await Package.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            DocumentView documentView = await VS.Documents.GetActiveDocumentViewAsync();
+            if(documentView == null)
+            {
+                return;
+            }
+
+            SnapshotPoint position = documentView.TextView.Caret.Position.BufferPosition;
+            try
+            {
+                var selectedText = documentView.TextBuffer.CurrentSnapshot.GetText(documentView.TextView.Selection.SelectedSpans.First());
+                var translatedText = await TranslateTextAsync(selectedText);
+
+                documentView.TextBuffer?.Insert(position, translatedText);
+            }
+            catch (Exception ex)
+            {
+                await VS.MessageBox.ShowErrorAsync($"Error during translation of the text: {Environment.NewLine}{ex.Message}");
+            }
+        }
+
+        private async Task<string> TranslateTextAsync(string sourceText)
+        {
+            var sourceLang = "en";
+            var targetLang = "de";
+            HttpClient httpClient = new HttpClient();
+
+            var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + Uri.EscapeDataString(sourceText);
+
+            var result = await httpClient.GetAsync(url);
+            if (!result.IsSuccessStatusCode)
+            {
+                return sourceText;
+            }
+
+            var content = await result.Content.ReadAsStringAsync();
+            var jsonDocument = JsonDocument.Parse(content);
+
+            var translatedElement = jsonDocument
+                .RootElement
+                .EnumerateArray()
+                .FirstOrDefault() // get first element from root array
+                .EnumerateArray()
+                .FirstOrDefault() // get first element from first inner array
+                .EnumerateArray()
+                .FirstOrDefault()
+                .ToString();
+
+            return translatedElement ?? sourceText;
+        }
+    }
+}
---
+++ TranslateExtension/README.md
@@ -0,0 +1,17 @@
+﻿> **Work still in progres**
+
+# About
+
+This extension is purposed to easily translate text to different languages right from the visual studio.
+
+Google translate is used as translator engine.
+
+## Future features
+
+1. Supported languages:
+    1. English - en
+    2. German - ge
+    3. Italian - it
+    4. French - fr
+2. Translate all related resource files (find by name pattern "{name}.{lang}.rsx")
+3. Translate selected text (Automatically replace)
